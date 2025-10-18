0aade285b3dec468e111500a62016a222434845c
maksudbhuiyan595 <mdmaksudbhuiyan595@gmail.com>
2025-10-18 12:23:30 +0600

    remove cros

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use PhpParser\Node\Stmt\TraitUseAdaptation\Alias;
-use App\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,17 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'admin' => AdminMiddleware::class,
-        'user' => UserMiddleware::class,
-        'cors' => \App\Http\Middleware\HandleCors::class,
-    ]);
-
-    // If you want it globally for all requests:
-    $middleware->prepend(\App\Http\Middleware\HandleCors::class);
-})
-
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'admin'=> AdminMiddleware::class,
+            'user'=> UserMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
